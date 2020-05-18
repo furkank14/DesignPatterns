c7f21bca59fbed3b136b316ae09cd255833369bf
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-18 22:31:05 +0300

    Created Project Bridge

---
+++ Bridge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class MessageManager
+    {
+        public void Save()
+        {
+            Console.WriteLine("Message Saved");
+        }
+
+        public void Send()
+        {
+
+        }
+    }
+}
