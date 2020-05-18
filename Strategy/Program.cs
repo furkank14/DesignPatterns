bfac066118dbf33f423dfa51d5f133895ce8fb32
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-18 23:34:28 +0300

    Strategy project in added codes

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -10,6 +10,46 @@ namespace Strategy
     {
         static void Main(string[] args)
         {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase=new After2010CreditCalculator();
+            customerManager.SaveCredit();
+
+            customerManager.CreditCalculatorBase = new Before2010CreditCalculator();
+            customerManager.SaveCredit();
+
+            Console.ReadLine();
+
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2010CreditCalculator:CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Before 2010 caltulated");
+        }
+    }
+
+    class After2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("After 2010 caltulated");
+        }
+    }
+
+    class CustomerManager
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer Manager Business");
+            CreditCalculatorBase.Calculate();
         }
     }
 }
