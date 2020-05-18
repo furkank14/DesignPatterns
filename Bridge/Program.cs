c4a96309896c4134c60cf8a490515f0c68b0fc40
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-18 22:45:59 +0300

    Bridge project added MessageSender Codes

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -10,19 +10,54 @@ namespace Bridge
     {
         static void Main(string[] args)
         {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.MessageSenderBase =new SmsSender();
+            customerManager.UpdateCustomer();
+
+            Console.ReadLine();
+
         }
     }
 
-    class MessageManager
+    abstract class MessageSenderBase
     {
         public void Save()
         {
             Console.WriteLine("Message Saved");
         }
 
-        public void Send()
-        {
+        public abstract void Send(Body body);
+    }
 
+    class Body
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+
+    class MailSender: MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sender MailSender",body.Title);
+        }
+    }
+
+    class SmsSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sender SmsSender", body.Title);
+        }
+    }
+
+    class CustomerManager
+    {
+        public MessageSenderBase MessageSenderBase { get; set; }
+        public void UpdateCustomer()
+        {
+            MessageSenderBase.Send(new Body{Title = "About the Course"});
+            Console.WriteLine("Customer Updated");
         }
     }
 }
