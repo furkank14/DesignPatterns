c5db79e1dbf2665b0b30237627eccb98e07ec536
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-19 23:21:52 +0300

    Project Commend in added new codes

--- Commend/Program.cs
+++ Commend/Program.cs
@@ -10,6 +10,91 @@ namespace Commend
     {
         static void Main(string[] args)
         {
+            StockManager stockManager = new StockManager();
+            BuyStock buyStock = new BuyStock(stockManager);
+            SellStock sellStock = new SellStock(stockManager);
+
+            StockController stockController = new StockController();
+            stockController.TakeOrder(buyStock);
+            stockController.TakeOrder(sellStock);
+            stockController.TakeOrder(buyStock);
+
+            stockController.PlaceOrder();
+
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class StockManager
+    {
+        private string _name = "Leptop";
+        private int _quantity = 10;
+
+
+        public void Buy()
+        {
+            Console.WriteLine("Stock {0},{1} BUY",_name,_quantity);
+        }
+
+        public void Sell()
+        {
+            Console.WriteLine("Stock {0},{1} SELL", _name, _quantity);
+        }
+    }
+
+    interface IOrder
+    {
+        void Execute();
+    }
+    class BuyStock:IOrder
+    {
+        private StockManager _stockManager;
+
+        public BuyStock(StockManager stockManager)
+        {
+            _stockManager = stockManager;
+        }
+
+        public void Execute()
+        {
+            _stockManager.Buy();
+        }
+    }
+
+    class SellStock : IOrder
+    {
+        private StockManager _stockManager;
+
+        public SellStock(StockManager stockManager)
+        {
+            _stockManager = stockManager;
+        }
+
+        public void Execute()
+        {
+            _stockManager.Sell();
+        }
+    }
+
+    class StockController
+    {
+        List<IOrder> _orders = new List<IOrder>();
+
+        public void TakeOrder(IOrder order)
+        {
+            _orders.Add(order);
+        }
+
+        public void PlaceOrder()
+        {
+            foreach (var order in _orders)
+            {
+                order.Execute();
+            }
+
+            _orders.Clear();
         }
     }
 }
