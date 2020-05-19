706c2db3568d1821c7c47f615597600e24f6b0dd
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-19 15:11:46 +0300

    Project "State" in added new codes

--- State/Program.cs
+++ State/Program.cs
@@ -10,6 +10,76 @@ namespace State
     {
         static void Main(string[] args)
         {
+            Context context = new Context();
+            ModifiedState modifiedState = new ModifiedState();
+            modifiedState.DoAction(context);
+            DeletedState deletedState = new DeletedState();
+            deletedState.DoAction(context);
+            Console.WriteLine(context.GetState());
+            Console.ReadLine();
+
+        }
+    }
+
+    interface IState
+    {
+        void DoAction(Context context);
+    }
+
+    class ModifiedState:IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State: Modified");
+            context.SetSate(this);
+        }
+
+        public override string ToString()
+        {
+            return "Modifiy";
+        }
+    }
+
+    class DeletedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State: Deleted");
+            context.SetSate(this);
+        }
+
+        public override string ToString()
+        {
+            return "Deleted";
+        }
+    }
+
+    class AddedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State: Added");
+            context.SetSate(this);
+        }
+
+        public override string ToString()
+        {
+            return "Added";
+        }
+    }
+
+    class Context
+    {
+        private IState _state;
+
+        public void SetSate(IState state)
+        {
+            _state = state;
+        }
+
+        public IState GetState()
+        {
+            return _state;
         }
     }
 }
