e19003371b2ae0a2e57820a7e4cf702a43ced4bb
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-23 03:06:29 +0300

    Project "NullObject" in added new codes

--- NullObject/Program.cs
+++ NullObject/Program.cs
@@ -10,6 +10,83 @@ namespace NullObject
     {
         static void Main(string[] args)
         {
+            //CustomerManager customerManager = new CustomerManager(new Log4NetLogger());
+            CustomerManager customerManager = new CustomerManager(StubLogger.GetLogger());
+            customerManager.Save();
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class CustomerManager
+    {
+        private ILogger _logger;
+
+        public CustomerManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            _logger.Log();
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class Log4NetLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Log with Log4Net");
+        }
+    }
+
+    class NLogLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Log with NLog");
+        }
+    }
+
+    class StubLogger:ILogger
+    {
+        private static StubLogger _stubLogger;
+        private static object _lock = new object();
+
+        private StubLogger() { }
+
+        public static StubLogger GetLogger()
+        {
+            lock (_lock)
+            {
+                if (_stubLogger==null)
+                {
+                    _stubLogger=new StubLogger();
+                }
+            }
+
+            return _stubLogger;
+        }
+        public void Log()
+        {
+            
+        }
+    }
+
+    class CustomerManagerTests
+    {
+        public void SaveTest()
+        {
+            CustomerManager customerManager = new CustomerManager(StubLogger.GetLogger());
+            customerManager.Save();
         }
     }
 }
