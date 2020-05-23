df68cfbab75e0d265a7ed86aeeca4e934ff56bd9
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-23 03:37:26 +0300

    Project "Multiton" added new codes

--- Multiton/Program.cs
+++ Multiton/Program.cs
@@ -10,6 +10,42 @@ namespace Multiton
     {
         static void Main(string[] args)
         {
+            Camera camera1 = Camera.GetCamera("NIKON");
+            Camera camera2 = Camera.GetCamera("NIKON");
+            Camera camera3 = Camera.GetCamera("CANON");
+            Camera camera4 = Camera.GetCamera("CANON");
+
+            Console.WriteLine(camera1.Id);
+            Console.WriteLine(camera2.Id);
+            Console.WriteLine(camera3.Id);
+            Console.WriteLine(camera4.Id);
+            Console.ReadLine();
+
+        }
+    }
+
+    class Camera
+    {
+        static Dictionary<string,Camera> _cameras = new Dictionary<string, Camera>();
+        static object _lock = new object();
+        public Guid Id { get; set; }
+        private Camera()
+        {
+           Id=Guid.NewGuid();
+
+        }
+
+        public static Camera GetCamera(string brand)
+        {
+            lock (_lock)
+            {
+                if (!_cameras.ContainsKey(brand))
+                {
+                    _cameras.Add(brand,new Camera());
+                }
+            }
+
+            return _cameras[brand];
         }
     }
 }
